80247905f0dba72c522112e8d72646764436688c
xyrlan <lobatopedro68@gmail.com>
2024-05-10 11:02:35 -0400

    Update HeroSection component text to fix typo

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
     <main className="flex h-screen flex-col items-center justify-between">
       <ImagesSliderProvider>
         <h1 className="font-bold text-4xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-400 py-4">
-          Consultoria de valuation e venda<br /> indústrias, empresas e startups.
+          Consultoria de valuation e venda<br />de indústrias, empresas e startups.
         </h1>
         <p className="font-medium max-w-2xl text-lg md:text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b to-white from-neutral-400 py-4">
          Especializados em valuation e venda de negócios, temos o comprador certo para continuar o seu negócio para que você prospere em novos desafios.
