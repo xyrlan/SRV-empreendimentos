b6910de8732d1445ef6cbd038688c8709f4058d7
xyrlan <lobatopedro68@gmail.com>
2024-05-10 11:11:36 -0400

    chore: Add motion values and animations to StatComponent

--- src/components/stat-component.tsx
+++ src/components/stat-component.tsx
@@ -4,25 +4,25 @@ import { useEffect } from 'react'
 
 export function StatComponent() {
   const count = useMotionValue(0);
+  const count2 = useMotionValue(0);
+  const count3 = useMotionValue(0);
   const rounded = useTransform(count, Math.round);
+  const rounded2 = useTransform(count2, Math.round);
+  const rounded3 = useTransform(count3, Math.round);
 
   useEffect(() => {
     const animation = animate(count, 126, { duration: 10 });
-
-    return animation.stop;
+    const animation2 = animate(count2, 5, { duration: 3 });
+    const animation3 = animate(count3, 7, { duration: 3 });
+    return () => { 
+      animation.stop();
+      animation2.stop();
+      animation3.stop();
+    }
   }, []);
   return (
     <section className="">
       <div className="mx-auto px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-        {/* <div className="mx-auto max-w-3xl text-center">
-          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">Trusted by eCommerce Businesses</h2>
-
-          <p className="mt-4 text-gray-300 sm:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
-            provident impedit esse recusandae facere libero harum sequi.
-          </p>
-        </div> */}
-
         <div className="mt-8 sm:mt-12">
           <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:divide-x sm:divide-gray-300">
             <div className="flex flex-col px-4 py-8 text-center">
@@ -39,13 +39,19 @@ export function StatComponent() {
             <div className="flex flex-col px-4 py-8 text-center">
               <dt className="order-last text-lg font-medium text-gray-300">Estados de atuação</dt>
 
-              <dd className="text-4xl font-extrabold text-white md:text-5xl">+ 5</dd>
+              <dd className="text-4xl font-extrabold text-white md:text-5xl">
+                <span>+</span>
+                <motion.span>{rounded2}</motion.span>
+              </dd>
             </div>
 
             <div className="flex flex-col px-4 py-8 text-center">
               <dt className="order-last text-lg font-medium text-gray-300">negocios vendidos</dt>
 
-              <dd className="text-4xl font-extrabold text-white md:text-5xl">+ 7</dd>
+              <dd className="text-4xl font-extrabold text-white md:text-5xl">
+                <span>+</span>
+                <motion.span>{rounded3}</motion.span>
+              </dd>
             </div>
           </dl>
         </div>
